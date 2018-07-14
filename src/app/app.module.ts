22c5cd3306fe14c9e2e47765df88c8c0b5886f6f
anish <anishkmathew@gmail.com>
2018-07-14 10:18:03 +0530

    Project Header, components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ManageShoppingListComponent } from './shopping-list/manage-shopping-list/manage-shopping-list.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipieListComponent } from './recipies/recipie-list/recipie-list.component';
+import { RecipieItemComponent } from './recipies/recipie-list/recipie-item/recipie-item.component';
+import { RecipieDetailsComponent } from './recipies/recipie-details/recipie-details.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ManageShoppingListComponent,
+    RecipiesComponent,
+    RecipieListComponent,
+    RecipieItemComponent,
+    RecipieDetailsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shopping-list/manage-shopping-list/manage-shopping-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-shopping-list',
+  templateUrl: './manage-shopping-list.component.html',
+  styleUrls: ['./manage-shopping-list.component.css']
+})
+export class ManageShoppingListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
